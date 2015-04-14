342fdaf8e2bcd9a682a0920f04f35620a2425193
bo.jiang <bo.jiang@finisar.com>
2015-04-14 19:34:45 +0800

    BUG FIXED ： 杠牌重新竖起

--- game/NetRaceRound.cpp
+++ game/NetRaceRound.cpp
@@ -989,7 +989,7 @@ ACT_RES NetRRound::others_action(unsigned char who_give,ARRAY_ACTION act,Card_t 
 	temp_data.kind = (CARD_KIND)kind;
 
 	if(act==a_PENG) {
-        card_delete(0,3);
+        card_delete(_cardInHand->active_place,3);
 
         CardNode_t node;
         node.kind=kind;
