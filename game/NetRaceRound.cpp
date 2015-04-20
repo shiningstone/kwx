b2b8c6f4eb1bbeee6a0b1dd07e451bb9ba35f1bb
bo.jiang <bo.jiang@finisar.com>
2015-04-21 00:19:13 +0800

    Revert "init of NetRaceRound"
    This reverts commit 62cc8e33dcb6ce0854b078e3c086bc955b298283.

--- game/NetRaceRound.cpp
+++ game/NetRaceRound.cpp
@@ -277,10 +277,11 @@ unsigned int NetRRound::ming_check()
     return handoutMask;
 }
 
-unsigned char NetRRound::init(int card_array[],int len)
+unsigned char NetRRound::init(int card_array[],int len,int aim)
 {
 	int i;
 
+	_AIM = aim;
 	rr_ting_flag = 0;
 	card_score=0;
 	hu_len=0;
