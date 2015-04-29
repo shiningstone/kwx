370301f39776d4c4932bf4939c20302796989537
bo.jiang <bo.jiang@finisar.com>
2015-04-29 17:55:40 +0800

    TestClient convenience

--- tester/network/SoloPrj_Client.cpp
+++ tester/network/SoloPrj_Client.cpp
@@ -6,7 +6,10 @@
 typedef enum {
     HEX,
     STRING,
+	IGNORE_RESPONSE,
 }MessageType_t;
+
+void GetArray(char *buf, int line);
 
 static int ctoi(char c) {
     if( c>='0' && c <= '9' ) {
@@ -31,25 +34,24 @@ void StringToHex(char *buf) {
 }
 
 int ArrayToSend[15][128] = {
-    { 0x4b,0x57,0x58,0x10,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x00,0x2c,0x00,0x2d,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x02,0x84,0x00,0x04,0x01,0x02,0x03,0x04,0x83,0x00,0x04,0x05,0x06,0x07,0x08 },
+    { 0x4b,0x57,0x58,0x10,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x00,0x2c,0x00,0x2d,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x02,0x84,0x00,0x04,0x01,0x02,0x03,0x04,0x83,0x00,0x04,0x00,0x00,0x00,0x0b },
 };
 
-void GetArray(char *buf, int line) {
-    memset(buf,0,128);
-    
-    for(int i=0;i<128;i++) {
-        buf[i] = ArrayToSend[line][i];
-    }
-}
-
 void test_client_console() {
+#ifdef REQUIRE_SERVER_IP
+	char serverIp[32] = {0};
+#else
+	char serverIp[32] = "10.101.104.116";
+#endif
+
     ClientSocket CLIENT;
     bool start = false;
     int  choice = STRING;
-    char serverIp[32] = {0};
 
-    printf("please input the IP address of the server:\n");
-    scanf("%s",&serverIp);
+	if(strlen(serverIp)==0) {
+		printf("please input the IP address of the server:\n");
+		scanf("%s",&serverIp);
+	}
     printf("please select the message format(0-HEX, 1-String, 2-String and only send):\n");
     scanf_s("%d",&choice);
 
@@ -87,7 +89,8 @@ void test_client_console() {
             } else {
                 CLIENT.Send(buf,128);
             }
-            if(choice!=2) {
+
+            if(choice!=IGNORE_RESPONSE) {
             	CLIENT.Recv(_clientBuff,&_clientBuffLen);
             }
         } else {
@@ -99,3 +102,12 @@ void test_client_console() {
 
 	CLIENT.Stop();
 }
+
+void GetArray(char *buf, int line) {
+    memset(buf,0,128);
+    
+    for(int i=0;i<128;i++) {
+        buf[i] = ArrayToSend[line][i];
+    }
+}
+
