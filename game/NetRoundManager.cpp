a5c0e3db8dc8b2ad61717802ef1f9d1fda3a5482
bo.jiang <bo.jiang@finisar.com>
2015-05-15 19:36:35 +0800

    BUG FIXED: 前一修改并不能解决问题

--- game/NetRoundManager.cpp
+++ game/NetRoundManager.cpp
@@ -603,7 +603,7 @@ void NetRoundManager::RecvHu(PlayerDir_t dir) {
 }
 
 Card_t NetRoundManager::RecvGang(PlayerDir_t dir) {
-    if(_actionToDo & aMING_GANG) {
+    if(_tempActionToDo & aMING_GANG) {
         _players[dir]->_cards->push_back(LastHandout());
     }
 
