ea2cbfece2b356df1a071f3a219b3f7b5db77a54
bo.jiang <bo.jiang@finisar.com>
2015-06-02 19:00:23 +0800

    BUG FIXED 明牌提示

--- game/RaceLayer.cpp
+++ game/RaceLayer.cpp
@@ -4454,7 +4454,7 @@ void RaceLayer::BtnMingHandler(cocos2d::Ref* pSender,cocos2d::ui::Widget::TouchE
                         ScaleTo::create(0.3,1.3),NULL),NULL)),NULL),CCCallFunc::create(this,callfunc_selector(
                 RaceLayer::_DeleteActionTip)),CCCallFunc::create([=](){
                 _CancelChosenCardInHand();
-                _roundManager->RecvMing(true);}),NULL));
+                _roundManager->RecvMing();}),NULL));
 		}
 		break;
 	case cocos2d::ui::Widget::TouchEventType::CANCELED:
